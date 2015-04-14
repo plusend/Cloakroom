f19adf87cfa19572874290d20a902e288d4691ba
Zhang Jian <jian.zhang@yulore.com>
2015-04-14 20:53:03 +0800

    Cloakroom init

---
+++ app/src/main/java/com/plusend/cloakroom/AppAdapter.java
@@ -0,0 +1,48 @@
+package com.plusend.cloakroom;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by yulore on 2015/4/14.
+ */
+public class AppAdapter extends RecyclerView.Adapter<AppAdapter.ViewHolder> {
+
+    private List<AppInfo> appInfoList;
+
+    public AppAdapter(List<AppInfo> list){
+        appInfoList = list;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.card_view, viewGroup, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder((TextView)v.findViewById(R.id.name));
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        viewHolder.mTextView.setText(appInfoList.get(position).getAppLabel());
+    }
+
+    @Override
+    public int getItemCount() {
+        return appInfoList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+}
---
+++ app/src/main/java/com/plusend/cloakroom/AppListFragment.java
@@ -0,0 +1,89 @@
+package com.plusend.cloakroom;
+
+import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by yulore on 2015/4/14.
+ */
+public class AppListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<AppInfo> mListAppInfo;
+
+    public AppListFragment() {
+        super();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+        // improve performance if you know that changes in content
+        // do not change the size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this.getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mListAppInfo = new ArrayList<AppInfo>();
+        queryAppInfo();
+
+        mAdapter = new AppAdapter(mListAppInfo);
+        mRecyclerView.setAdapter(mAdapter);
+        return rootView;
+    }
+
+    private void queryAppInfo() {
+        PackageManager pm = this.getActivity().getPackageManager(); // 获得PackageManager对象
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        // 通过查询，获得所有ResolveInfo对象.
+        List<ResolveInfo> resolveInfos = pm
+                .queryIntentActivities(mainIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        // 调用系统排序 ， 根据name排序
+        // 该排序很重要，否则只能显示系统应用，而不能列出第三方应用程序
+        Collections.sort(resolveInfos, new ResolveInfo.DisplayNameComparator(pm));
+        if (mListAppInfo != null) {
+            mListAppInfo.clear();
+            for (ResolveInfo reInfo : resolveInfos) {
+                String activityName = reInfo.activityInfo.name; // 获得该应用程序的启动Activity的name
+                String pkgName = reInfo.activityInfo.packageName; // 获得应用程序的包名
+                String appLabel = (String) reInfo.loadLabel(pm); // 获得应用程序的Label
+                Drawable icon = reInfo.loadIcon(pm); // 获得应用程序图标
+                // 为应用程序的启动Activity 准备Intent
+                Intent launchIntent = new Intent();
+                launchIntent.setComponent(new ComponentName(pkgName,
+                        activityName));
+                // 创建一个AppInfo对象，并赋值
+                AppInfo appInfo = new AppInfo();
+                appInfo.setAppLabel(appLabel);
+                appInfo.setPkgName(pkgName);
+                appInfo.setAppIcon(icon);
+                appInfo.setIntent(launchIntent);
+                mListAppInfo.add(appInfo); // 添加至列表中
+                System.out.println(appLabel + " activityName---" + activityName
+                        + " pkgName---" + pkgName);
+            }
+        }
+    }
+
+}
+
